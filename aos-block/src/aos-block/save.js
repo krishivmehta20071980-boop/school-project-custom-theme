8cad28591631c1367213ba1d97ed76ccef7a5221
ryleviathan <ryleviathan@gmail.com>
2026-03-11 19:27:07 -0700

    finalized code for aos block

--- aos-block/src/aos-block/save.js
+++ aos-block/src/aos-block/save.js
@@ -1,14 +1,17 @@
 import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 
-export default function save({ attributes }) {
-    const blockProps = useBlockProps.save({
-        'data-aos': attributes.animationType,
-        'className': 'aos-wrapper'
-    });
+export default function save( { attributes } ) {
+const { animationType } = attributes;
 
-    return (
-        <div { ...blockProps }>
-            <InnerBlocks.Content />
-        </div>
-    );
+const blockProps = useBlockProps.save({
+'data-aos': animationType,
+'className': 'aos-wrapper'
+});
+
+return (
+
+<div { ...blockProps }>
+<InnerBlocks.Content />
+</div>
+);
 }
